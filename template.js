6df4d05b841d95461d359ffa4ad3c9b0783ac250
Gatsby <87589924+BrandonOkman@users.noreply.github.com>
2022-10-06 12:23:34 -0600

    input code in wrong file

---
+++ template.js
@@ -0,0 +1,36 @@
+function generateMarkdown(data) {
+    return `# ${data.title}
+  
+    ## Description
+    ${data.description}
+  
+    ## Table of Contents
+    * [Installation](#installation)
+    * [Usage](#usage)
+    * [License](#license)
+    * [Contributing](#contributing)
+    * [Tests](#tests)
+    * [Questions](#questions)
+  
+    ## Installation
+      ${data.installation}
+  
+  ## Usage
+    ${data.usage}
+  
+  ## License  
+    ${data.license}
+  
+  ## Contributing
+      ${data.contributing}
+  
+  ## Tests
+      ${data.tests}
+  
+  ## Questions
+    If you have any questions about the repo contact me directly at ${data.email}. You can find more of my work at ${data.github}
+  
+  `; 
+  }   
+  
+  module.exports = generateMarkdown;
